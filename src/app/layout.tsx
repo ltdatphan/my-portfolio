b0e587ee163e1ac6b2346a938c700d4ace0652d9
David Phan <29266892+ltdatphan@users.noreply.github.com>
2023-05-25 11:55:16 -0400

    update opengraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter, IBM_Plex_Serif } from "next/font/google";
+import type { Metadata } from "next";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -14,29 +15,28 @@ const ibmPlexSerif = IBM_Plex_Serif({
   variable: "--font-ibm-plex-serif",
 });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "David Phan | Portfolio",
   description: "David Phan's Personal Portfolio",
-  url: "https://davidphan.dev",
   generator: "Next.js",
   applicationName: "Next.js",
   referrer: "origin-when-cross-origin",
   keywords: ["Next.js", "React", "JavaScript"],
   icons: {
-    favicon: "/favicon.ico",
     icon: "/favicon.ico",
     apple: "/apple-touch-icon.png",
   },
-  images: [
-    {
-      url: "https://davidphan.dev/preview.webp",
-      width: 1200,
-      height: 600,
-    },
-  ],
   openGraph: {
     title: "David Phan | Portfolio",
     description: "David Phan's Personal Portfolio",
+    url: "https://davidphan.dev",
+    images: [
+      {
+        url: "https://davidphan.dev/preview.webp",
+        width: 1200,
+        height: 600,
+      },
+    ],
   },
 };
 
