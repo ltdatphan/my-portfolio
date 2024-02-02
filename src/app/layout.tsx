3ddd0c84fbb07d6daad60237e61a895169e8a4db
David Phan <29266892+ltdatphan@users.noreply.github.com>
2024-02-02 07:26:44 +0700

    added analytics and speed info

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter, IBM_Plex_Serif } from 'next/font/google'
 import type { Metadata } from 'next'
 import Providers from '@/contexts/Providers'
 import Head from 'next/head'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const inter = Inter({
     subsets: ['latin'],
@@ -67,6 +69,8 @@ export default function RootLayout({
         <html lang="en" suppressHydrationWarning>
             <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>
                 <Providers>{children}</Providers>
+                <Analytics />
+                <SpeedInsights />
             </body>
         </html>
     )
